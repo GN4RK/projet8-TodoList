ad7c9908852968deeb7f3a0d27131452260a0854
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-09-26 16:45:19 +0200

    renamed controller functions

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ class DefaultController extends AbstractController
 {
 
     #[Route('/', name: 'homepage')]
-    public function indexAction()
+    public function index()
     {
         return $this->render('default/index.html.twig');
     }
